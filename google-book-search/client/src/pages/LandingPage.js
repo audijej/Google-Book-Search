55863f89f00114a4870b361f8ec07c46a017edcb
Jesse Audije <audijej@gmail.com>
2020-08-27 19:00:31 -0700

    updated landing page css and reroutes

--- google-book-search/client/src/pages/LandingPage.js
+++ google-book-search/client/src/pages/LandingPage.js
@@ -1,22 +1,35 @@
 import React, { Component } from "react";
 import "../components/style.css";
 import SearchBook from "./SearchBook";
-import {List, ListItem} from "../components/List/List"
+import { List, ListItem } from "../components/List/List"
 import Row from 'react-bootstrap/Row';
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import SearchForm from '../components/Form/SearchForm';
-import {DeleteBtn, ViewBtn, SaveBtn} from '../components/Button/Btn'
+import { DeleteBtn, ViewBtn, SaveBtn } from '../components/Button/Btn'
 import ViewBook from "./ViewBook";
+import Button from 'react-bootstrap/Button';
+import { Link } from "react-router-dom";
+
+
 
 
 
 function LandingPage() {
-    return (
-        <div >
 
-            <img src = "https://external-preview.redd.it/yZc0bxK3qvWcx1bfWxn-Sahbrw346a8GjeRgx2CDDWk.jpg?auto=webp&s=e65d09485c4c62c8b8a914e06dbf4ddb4a12106a" />
+    return (
+        <div className="bg">
+            <div class="hero-text">
+                <h1>Google Book Search Application</h1>
+                <p id="landingPageText">Search for books on google and save them to you list</p>
+                <Button><Link
+    to="/searchbook"
+    className={window.location.pathname === "/searchbook" ? "nav-link active" : "nav-link"}
+            >
+    <h1>Search a book</h1>
+    </Link></Button>
+            </div>
         </div>
     )
 }
--- google-book-search/client/src/pages/ViewBook.js
+++ google-book-search/client/src/pages/ViewBook.js
@@ -1,11 +1,11 @@
 import "../components/style.css";
 import SearchBook from "./SearchBook";
-import {List, ListItem} from "../components/List/List"
+import { List, ListItem } from "../components/List/List"
 import Row from 'react-bootstrap/Row';
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Jumbotron from 'react-bootstrap/Jumbotron';
-import {DeleteBtn, ViewBtn, SaveBtn} from '../components/Button/Btn';
+import { DeleteBtn, ViewBtn, SaveBtn } from '../components/Button/Btn';
 import API from "../utils/API";
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
@@ -14,104 +14,103 @@ import axios from 'axios';
 
 
 
-function ViewBook () {
+function ViewBook() {
 
-    const [savedbooks, setSaveBooks] = useState([])
-    const [formObject, setFormObject] = useState({})
-    const [books, setBooks] = useState('')
-    const [bookList, setbookList] = useState({listing: []})
+  const [savedbooks, setSaveBooks] = useState([])
+  const [formObject, setFormObject] = useState({})
+  const [books, setBooks] = useState('')
+  const [bookList, setbookList] = useState({ listing: [] })
 
 
-    useEffect(() => {
-        loadBooks()
-    }, [])
+  useEffect(() => {
+    loadBooks()
+  }, [])
 
-    // function searchForBook(event) {
-    //     event.preventDefault();
-    //     const query =  books;
-    //     const BASEURL = `https://www.googleapis.com/books/v1/volumes?q=`
-    //     axios.get(BASEURL + query)
-    //         .then(res => {
-    //             console.log(res.data.items)
-    //             console.log("hello")
-    //             setbookList({listing: res.data.items})
-    //             setFormObject()
-    //         })
-    // }
+  // function searchForBook(event) {
+  //     event.preventDefault();
+  //     const query =  books;
+  //     const BASEURL = `https://www.googleapis.com/books/v1/volumes?q=`
+  //     axios.get(BASEURL + query)
+  //         .then(res => {
+  //             console.log(res.data.items)
+  //             console.log("hello")
+  //             setbookList({listing: res.data.items})
+  //             setFormObject()
+  //         })
+  // }
 
-    function loadBooks() {
-        API.getBooks()
-            .then(res =>
-                setSaveBooks(res.data)
-            )
-            .catch(err => console.log(err));
-    };
+  function loadBooks() {
+    API.getBooks()
+      .then(res =>
+        setSaveBooks(res.data)
+      )
+      .catch(err => console.log(err));
+  };
 
-    function deleteBook(id) {
-        API.deleteBook(id)
-            .then(res => loadBooks())
-            .catch(err => console.log(err));
-    }
+  function deleteBook(id) {
+    API.deleteBook(id)
+      .then(res => loadBooks())
+      .catch(err => console.log(err));
+  }
 
-    // function handleInputChange(event) {
-    //     const { name, value } = event.target;
-    //     setFormObject({ ...formObject, [name]: value })
-    // };
+  // function handleInputChange(event) {
+  //     const { name, value } = event.target;
+  //     setFormObject({ ...formObject, [name]: value })
+  // };
 
-    // function handleFormSubmit(event) {
-    //     event.preventDefault();
-    //     if (formObject.title && formObject.author) {
-    //         API.saveBook({
-    //             title: formObject.title,
-    //             author: formObject.author,
-    //             synopsis: formObject.synopsis
-    //         })
-    //             .then(res => loadBooks())
-    //             .catch(err => console.log(err));
-    //     }
-    // };
+  // function handleFormSubmit(event) {
+  //     event.preventDefault();
+  //     if (formObject.title && formObject.author) {
+  //         API.saveBook({
+  //             title: formObject.title,
+  //             author: formObject.author,
+  //             synopsis: formObject.synopsis
+  //         })
+  //             .then(res => loadBooks())
+  //             .catch(err => console.log(err));
+  //     }
+  // };
 
-    return (
-        
+  return (
 
-<div className="container">
+    <div>
 
-<Jumbotron fluid>
-    <Container>
-    <h1>View your saved books here</h1>
-        <p>
+      <Jumbotron fluid>
+        <Container>
+          <h1>View your saved books here</h1>
+          <p>
             Find a book today.
         </p>
-    </Container>
-</Jumbotron>
+        </Container>
+      </Jumbotron>
 
-<br></br>
-<br></br>
+      <div className="container">
 
-<Container fluid id="resultsdiv" >
-    <h1>Saved Books</h1>
-    {savedbooks.length ? (
-              <List>
-                {savedbooks.map(book => (
-                  <ListItem key={book._id}>
-                    <Link to={"/books/" + book._id}>
-                      <strong>
+        <Container fluid id="resultsdiv" >
+          <h1>Saved Books</h1>
+          {savedbooks.length ? (
+            <List>
+              {savedbooks.map(book => (
+                <ListItem key={book._id}>
+                  <Link to={"/books/" + book._id}>
+                    <strong>
                       {/* <a href= {savedbooks.volumeInfo.previewLink} target="_blank"  > */}
-                        {book.title} by {book.authors}
-                        {/* </a> */}
-                      </strong>
-                    </Link>
-                    <br></br>
-                    <DeleteBtn onClick={() => deleteBook(book._id)} />
-                  </ListItem>
-                ))}
-              </List>
-            ) : (
+                      {book.title} by {book.authors}
+                      {/* </a> */}
+                    </strong>
+                  </Link>
+                  <br></br>
+                  <DeleteBtn onClick={() => deleteBook(book._id)} />
+                </ListItem>
+              ))}
+            </List>
+          ) : (
               <h3>No Results to Display</h3>
             )}
-</Container>
-</div>
-    )
+        </Container>
+      </div>
+    </div>
+  )
 }
 
 export default ViewBook;
