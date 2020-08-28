87e4c557286fa65d0eeeea9afbbf2d214f4f5d71
Jesse Audije <audijej@gmail.com>
2020-08-27 21:44:16 -0700

    heroku launch attempt 2

--- google-book-search/server.js
+++ google-book-search/server.js
@@ -13,7 +13,8 @@ console.log("connect please")
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/googlebooks",
 { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true }
 );
-
+const mongo = process.env.MONGODB_URI
+mongoose.connect(mongo)
 console.log("connect connect please")
 
 
